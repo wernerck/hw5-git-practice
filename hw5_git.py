6e25b383e40bad45053d3bb5f983284d31e11ffc
Christian Werner <wernerck@umich.edu>
2020-10-07 00:21:48 -0400

    Adding hw5_git

---
+++ hw5_git.py
@@ -0,0 +1,11 @@
+# Christian Werner
+# wernerck
+# SI 507 - 003
+# Worked with: N/A
+
+print("Hello World")
+print("This is homework 5")
+
+colors = ["red", "blue", "green", "orange"]
+colors.append("pink")
+print(colors)
